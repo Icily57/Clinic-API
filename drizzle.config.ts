873b78ca5d8ec7e4d7803619151474c2a4dda831
Icily57 <icilyweird57@gmail.com>
2025-09-07 16:23:46 +0300

    Remove the database index file as it is no longer needed.

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,14 +1,16 @@
-import "dotenv/config"; // this automatically loads .env variables
+import "dotenv/config"; 
 import { defineConfig } from "drizzle-kit";
 
-console.log("Database_URL:", process.env.DATABASE_URL);
+if (!process.env.DATABASE_URL) {
+  throw new Error("❌ DATABASE_URL is not defined in .env");
+}
 
 export default defineConfig({
   schema: "./src/drizzle/schema.ts",
   out: "./src/drizzle/migrations",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DATABASE_URL as string, // make sure .env has DATABASE_URL
+    url: process.env.DATABASE_URL, // Neon connection string
   },
   verbose: true,
   strict: true,
